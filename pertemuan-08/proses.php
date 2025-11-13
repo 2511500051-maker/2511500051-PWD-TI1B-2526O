8eeeec501a5b3b3975a622b5b369b71255090ab9
2511500051@mahasiswa.atmaluhur.ac.id <2511500090@mahasiswa.atmaluhur.ac.id>
2025-11-13 12:59:11 +0700

    Menggunakan PHP dan session untuk pengiriman data

--- pertemuan-08/proses.php
+++ pertemuan-08/proses.php
@@ -1,10 +1,20 @@
 <?php
 session_start();
-$sesnama = $_POST["txtNama"];
-$sesemail = $_POST["txtEmail"];
-$sespesan = $_POST["txtPesan"];
-$_SESSION["sesnama"] = $sesnama;
-$_SESSION["sesemail"] = $sesemail;
-$_SESSION["sespesan"] = $sespesan;
-header("location: index.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nim = $_POST["txtNim"];
+  $_SESSION["nim"] = $nim;
+  $_SESSION["nama"] = $_POST["txtNama"];
+  $_SESSION["tempat"] = $_POST["txtTempat"];
+  $_SESSION["tanggal"] = $_POST["txtTanggal"];
+  $_SESSION["hobi"] = $_POST["txtHobi"];
+  $_SESSION["pasangan"] = $_POST["txtPasangan"];
+  $_SESSION["pekerjaan"] = $_POST["txtPekerjaan"];
+  $_SESSION["ortu"] = $_POST["txtOrtu"];
+  $_SESSION["kakak"] = $_POST["txtKakak"];
+  $_SESSION["adik"] = $_POST["txtAdik"];
+}
+
+header("Location: index.php#about");
+exit;
 ?>
