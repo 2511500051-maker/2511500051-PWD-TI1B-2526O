0965645ae9e09509745215c815a35fbf3281c653
cintia divanti agustin <2511500051@mahasiswa.atmaluhur.ac.id>
2025-11-16 00:43:07 +0700

    Start project

--- pertemuan-08/index.php
+++ pertemuan-08/index.php
@@ -147,6 +147,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $nama_kakak     = $_POST["nama_kakak"];
   $nama_adik      = $_POST["nama_adik"];
 
+   
+ if (!isset($_SESSION["history_mahasiswa"])) {
+   $_SESSION["history_mahasiswa"] = [];
+}
+
+   
+  $_SESSION["history_mahasiswa"][] = [
+    "nim"           => $_POST["nim"],
+    "nama"          => $_POST["nama"],
+    "tempat_lahir"  => $_POST["tempat_lahir"],
+    "tanggal_lahir" => $_POST["tanggal_lahir"],
+    "hobi"          => $_POST["hobi"],
+    "pasangan"      => $_POST["pasangan"],
+    "pekerjaan"     => $_POST["pekerjaan"],
+    "nama_ortu"     => $_POST["nama_ortu"],
+    "nama_kakak"    => $_POST["nama_kakak"],
+    "nama_adik"     => $_POST["nama_adik"]
+];
+
   $_SESSION["nim"]           = $nim;
   $_SESSION["nama_lengkap"]  = $nama_lengkap;
   $_SESSION["tempat_lahir"]  = $tempat_lahir;
@@ -158,7 +177,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $_SESSION["nama_kakak"]    = $nama_kakak;
   $_SESSION["nama_adik"]     = $nama_adik;
 
-  header("Location: index.php#about");
+  header("Location: index.php#data-mahasiswa");
   exit;
 }
 ?>
--- pertemuan-08/proses.php
+++ pertemuan-08/proses.php
@@ -14,6 +14,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $_SESSION["adik"]      = $_POST["nama_adik"];
 }
 
-header("Location: index.php#about");
+if (!isset($_SESSION["history_mahasiswa"])) {
+  $_SESSION["history_mahasiswa"] = [];
+ }
+
+  $_SESSION["history_mahasiswa"][] = [
+   "nim"           => $_POST["nim"],
+   "nama"          => $_POST["nama"],
+   "tempat_lahir"  => $_POST["tempat_lahir"],
+   "tanggal_lahir" => $_POST["tanggal_lahir"],
+   "hobi"          => $_POST["hobi"],
+   "pasangan"      => $_POST["pasangan"],
+   "pekerjaan"     => $_POST["pekerjaan"],
+   "nama_ortu"     => $_POST["nama_ortu"],
+   "nama_kakak"    => $_POST["nama_kakak"],
+   "nama_adik"     => $_POST["nama_adik"]
+];
+
+header("Location: index.php#data-mahasiswa");
 exit;
 ?>
