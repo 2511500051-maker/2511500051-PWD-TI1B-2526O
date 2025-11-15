1a7840bd23d2aea8595ee608d6bafc9e7f7aae54
cintia divanti agustin <2511500051@mahasiswa.atmaluhur.ac.id>
2025-11-16 00:02:53 +0700

    Start project

--- pertemuan-08/index.php
+++ pertemuan-08/index.php
@@ -52,7 +52,7 @@ endif;
       <p>Ini contoh paragraf HTML.</p>
     </section>
 
-    <section id="Data mahasiswa">
+    <section id="Data_mahasiswa">
       <h2>Entry Data Mahasiswa</h2>
       <form action="proses_entry.php" method="POST">
         <label for="nim">NIM:</label>
