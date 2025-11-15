12a2d7df4995ad47af3960c6b6261d74a3311b27
cintia divanti agustin <2511500051@mahasiswa.atmaluhur.ac.id>
2025-11-16 01:16:37 +0700

    Start project

--- pertemuan-08/index.php
+++ pertemuan-08/index.php
@@ -105,28 +105,28 @@ $kontak = [
 </section>
 
 <section id="contact">
-      <h2>Kontak Kami</h2>
-      <form action="" method="GET">
+<h2>Kontak Kami</h2>
+<form action="" method="GET">
 
-        <label for="txtNama"><span>Nama:</span>
-          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
-        </label>
+<label for="txtNama"><span>Nama:</span>
+<input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
+</label>
 
-        <label for="txtEmail"><span>Email:</span>
-          <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email" required autocomplete="email">
-        </label>
+<label for="txtEmail"><span>Email:</span>
+<input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email" required autocomplete="email">
+</label>
 
-        <label for="txtPesan"><span>Pesan Anda:</span>
-          <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
-          <small id="charCount">0/200 karakter</small>
-        </label>
+<label for="txtPesan"><span>Pesan Anda:</span>
+<textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
+<small id="charCount">0/200 karakter</small>
+</label>
 
 
-        <button type="submit">Kirim</button>
-        <button type="reset">Batal</button>
-      </form>
-    </section>
-  </main>
+<button type="submit">Kirim</button>
+<button type="reset">Batal</button>
+</form>
+</section>
+</main>
 
 <footer>
 <p>&copy; 2025 Cintia Divanti Agustin</p>
