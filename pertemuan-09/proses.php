5f0ce96cdd4823f4781e65f78826277ad9026ee2
cintia divanti agustin <2511500051@mahasiswa.atmaluhur.ac.id>
2025-11-27 11:56:47 +0700

    menambahkan fieldConfig

--- pertemuan-09/proses.php
+++ pertemuan-09/proses.php
@@ -8,19 +8,31 @@ $_SESSION["sesemail"] = $sesemail;
 $_SESSION["sespesan"] = $sespesan;
 
 $arrBiodata = [
-    "nim" => $_POST["txtNim"] ?? "",
-    "nama" => $_POST["txtNmLengkap"] ?? "",
-    "tempat" => $_POST["txtT4Lhr"] ?? "",
-    "tanggal" => $_POST["txtTglLhr"] ?? "",
-    "hobi" => $_POST["txtHobi"] ?? "",
-    "pasangan" => $_POST["txtPasangan"] ?? "",
-    "pekerjaan" => $_POST["txtKerja"] ?? "",
-    "ortu" => $_POST["txtNmOrtu"] ?? "",
-    "kakak" => $_POST["txtNmKakak"] ?? "",
-    "adik" => $_POST["txtNmAdik"] ?? "",
+"nim" => $_POST["txtNim"] ?? "",
+"nama" => $_POST["txtNmLengkap"] ?? "",
+"tempat" => $_POST["txtT4Lhr"] ?? "",
+"tanggal" => $_POST["txtTglLhr"] ?? "",
+"hobi" => $_POST["txtHobi"] ?? "",
+"pasangan" => $_POST["txtPasangan"] ?? "",
+"pekerjaan" => $_POST["txtKerja"] ?? "",
+"ortu" => $_POST["txtNmOrtu"] ?? "",
+"kakak" => $_POST["txtNmKakak"] ?? "",
+"adik" => $_POST["txtNmAdik"] ?? ""
 ];
 
-$_SESSION["biodata"] = $arrBiodata;
-header("location: index.php#about");
+foreach ($dataBiodata as $k => $v) {
+echo "<p><strong>$k</strong>: $v</p>";
+}
+
+$_SESSION["txtNim"] = $txtNim;
+$_SESSION["txtNmLengkap"] = $txtNmLengkap;
+$_SESSION["txtT4Lhr"] = $txtT4Lhr;
+$_SESSION["txtTglLhr"] = $txtTglLhr;
+$_SESSION["txtHobi"] = $txtHobi;
+$_SESSION["txtPasangan"] = $txtPasangan;
+$_SESSION["txtKerja"] = $txtKerja;
+$_SESSION["txtNmOrtu"] = $txtNmOrtu;
+$_SESSION["txtNmKakak"] = $txtNmKakak;
+$_SESSION["txtNmAdik"] = $txtNmAdik;
 header("location: index.php");
 ?>
